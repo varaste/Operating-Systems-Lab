47747f59c7e3565e0846c9849a828c731c38bbe2
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-06-26 12:38:07 +0430

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+
+	int i,num;
+	
+  	scanf("%d",&num);
+    for(i=1; i<num; i++){
+	printf("%S","man khoshghlab hastam");
+    }
+    
+	return 0;
+
+}
